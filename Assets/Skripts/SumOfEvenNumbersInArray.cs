f94a5dc0963e085c4339e0d75ebf0ab28770d2c0
Даниил <90823680+DanyWereWolf@users.noreply.github.com>
2024-11-11 10:42:42 +0700

    ver_0.2

--- Assets/Skripts/SumOfEvenNumbersInArray.cs
+++ Assets/Skripts/SumOfEvenNumbersInArray.cs
@@ -4,9 +4,8 @@ using UnityEngine.UI;
 
 public class SumOfEvenNumbersInArray : MonoBehaviour
 {
-    /// <summary>
-    /// Метод обработки события OnClick кнопки "Сумма четных чисел в заданном массиве"
-    /// </summary>
+    [SerializeField] private Text AnswerText;
+
     public void OnSumEvenNumbersInArray()
     {
         int[] array = { 81, 22, 13, 54, 10, 34, 15, 26, 71, 68 };
@@ -14,17 +13,20 @@ public class SumOfEvenNumbersInArray : MonoBehaviour
         int got = SumEvenNumbersInArray(array);
         string message = want == got ? "Результат верный" : $"Результат не верный, ожидается {want}";
         Debug.Log($"Сумма четных чисел в заданном массиве: {got} - {message}");
+        AnswerText.text = got.ToString();
     }
 
-    /// <summary>
-    /// Метод вычисляет сумму четных чисел в массиве 
-    /// </summary>
-    /// <param name="array">Исходный массив чисел</param>
-    /// <returns>Сумма чисел четных чисел</returns>
     private int SumEvenNumbersInArray(int[] array)
     {
-        // Реализуйте подсчет четных чисел используя цикл и
-        // верните вместо 0 полученный результат 
-        return 0;
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                sum += array[i];
+            }
+        }
+        return sum;
     }
 }
